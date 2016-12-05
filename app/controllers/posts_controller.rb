6c6866badca37117ef0686748f016827b821b5e7
Matthew Ramuta <matthew.g.ramuta@gmail.com>
2016-12-05 08:44:20 -0600

    add post controller code

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,22 @@
 class PostsController < ApplicationController
+
+
+  def create
+    @post = Post.new(post_params)
+    @post.save
+    redirect_to @post
+  end
+
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  def new
+  end
+
+  private
+  def post_params
+    params.require(:post).permit(:title,:body)
+  end
 end
+
